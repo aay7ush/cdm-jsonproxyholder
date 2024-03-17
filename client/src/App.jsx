79d356f63f4baf284638a004dc1250b0100478a3
aay7ush <aay7ush@gmail.com>
2024-03-17 21:21:21 +0530

    add starter files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,69 +1,8 @@
-import { useEffect, useState } from 'react'
-
 const App = () => {
-  const [posts, setPosts] = useState([])
-  const [selectedPost, setSelectedPost] = useState(null)
-  const [comments, setComments] = useState([])
-  const [showDetail, setShowDetail] = useState(false)
-
-  const API_BASE_URL = 'http://localhost:1338'
-
-  useEffect(() => {
-    fetch(`${API_BASE_URL}/posts`)
-      .then((res) => res.json())
-      .then((data) => setPosts(data))
-  }, [])
-
-  const handlePostClick = (postId) => {
-    fetch(`${API_BASE_URL}/posts/${postId}`)
-      .then((res) => res.json())
-      .then((post) => {
-        setSelectedPost(post)
-        fetch(`${API_BASE_URL}/posts/${postId}/comments`)
-          .then((res) => res.json())
-          .then((data) => setComments(data))
-      })
-    setShowDetail((prev) => !prev)
-  }
-
-  return (
-    <section className="max-w-3xl mx-auto my-10">
-      <h1 className="text-4xl font-bold text-center mb-2">Posts</h1>
-
-      <ul className="space-y-5">
-        {posts.map((post) => (
-          <li
-            key={post.id}
-            onClick={() => handlePostClick(post.id)}
-            className="rounded-lg shadow-md transition duration-300 hover:shadow-xl px-5 py-3"
-          >
-            <h2 className="text-xl font-medium text-indigo-600 hover:text-indigo-800 transition duration-300 cursor-pointer">
-              {post.title}
-            </h2>
-
-            {showDetail && selectedPost && selectedPost.id === post.id && (
-              <div className="my-2 space-y-3">
-                <p className="text-gray-700">{selectedPost.body}</p>
-                <h3 className="text-lg font-medium text-indigo-600">
-                  Comments:
-                </h3>
-                <ul className="space-y-3">
-                  {comments.map((comment) => (
-                    <li
-                      key={comment.id}
-                      className="bg-indigo-50 rounded-lg p-4"
-                    >
-                      {comment.body}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
-    </section>
-  )
+	return (
+		<h1 className="text-3xl font-bold underline">
+			Let's build JSON Proxy Holder
+		</h1>
+	)
 }
-
 export default App
--- server/server.js
+++ server/server.js
@@ -6,21 +6,9 @@ const app = express()
 
 app.use(cors())
 
-app.use((req, res, next) => {
-  res.set('x-codedamn-project', 'jsonproxyholder')
-  next()
-})
+app.use((req, res, next) => {})
 
-app.get('/*', async (req, res) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com${req.url}`)
-
-  if (response.ok) {
-    const data = await response.json()
-    res.json(data)
-  } else {
-    res.status(response.status).json({ error: 'Failed to fetch data' })
-  }
-})
+app.get('/*', async (req, res) => {})
 
 app.listen(1338, () => {
   console.log(`Server is running on port 1338`)
